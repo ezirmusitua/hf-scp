6ab067c7dedd1a5da2217420b0a57abfca8a0052
ezirmusitua <jferroal@gmail.com>
2020-05-07 18:51:24 +0800

    Feat: Layout Almost Done

---
+++ rollup.config.js
@@ -0,0 +1,12 @@
+import resolve from '@rollup/plugin-node-resolve';
+
+export default {
+  input: 'main.js',
+  output: {
+    file: 'bundle.js',
+    format: 'cjs',
+  },
+  plugins: [
+    resolve(),
+  ],
+};
